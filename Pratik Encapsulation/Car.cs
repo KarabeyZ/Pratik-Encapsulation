d5ffb6fb1165bfd32cccf923668a703b18dd5254
Zekeriya <Zekeriya@ZEKERIYA>
2024-08-02 14:41:26 +0300

    Add project files.

---
+++ Pratik Encapsulation/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Encapsulation
+{
+    public class Car
+    {
+        //Fieldlar private tanımlandı
+        private string _marka;
+        private string _model;
+        private string _renk;
+        private int _kapiSayisi;
+        //public property'lerle fieldlere erişim sağlanıyor
+        public string Marka { get { return _marka; } set { _marka = value; } }
+        public string Model { get { return _model; } set { _model = value; } }
+        public string Renk { get { return _renk; } set { _renk = value; } }
+        public int KapiSayisi
+        {
+            get { return _kapiSayisi; }
+            set
+            {
+                //girilen sayi 2-4 ise kapi sayisi alanina atatniyor
+                if (value == 2 || value == 4)
+                {
+                    _kapiSayisi = value;
+                }
+                else
+                {
+                    _kapiSayisi = -1;
+                    Console.WriteLine("Kapı sayısı 2 veya 4 olmalıdır.");
+                }
+            }
+        }
+        //car constructorı tanımlanıyor, car nesnesi oluşturulduğunda çağrılıyor.
+        public Car(string marka, string model, string renk, int kapiSayisi)
+        {
+            Marka = marka;
+            Model = model;
+            Renk = renk;
+            KapiSayisi = kapiSayisi;
+        }
+    }
+}
---
+++ Pratik Encapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿using Pratik_Encapsulation;
+//doğru kapı sayısı atanıyor
+Car araba = new Car("Honda","Civic","Beyaz",4);
+Console.WriteLine($"{araba.Marka},{araba.Model},{araba.Renk},{araba.KapiSayisi}");
+
+
+//yanlış kapı sayısı atanıyor
+Car araba2 = new Car("Toyota", "Corolla", "Siyah", 3);
+Console.WriteLine($"{araba2.Marka}, {araba2.Model}, {araba2.Renk}, {araba2.KapiSayisi}");
